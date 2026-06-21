0399828eaabf5c14f917e9eaba921c43b03d71cb
Terence Le Thierry <terence.lethierry@protonmail.com>
2026-06-21 15:17:51 +0200

    Affichage de la tête du serpent

---
+++ snake.gd
@@ -0,0 +1,59 @@
+extends Node2D
+
+
+# Récupérer les différents sprites de la tête du serpent
+
+@onready var head_sprite = $Head
+
+var head_up : Texture2D
+var head_down : Texture2D
+var head_left : Texture2D
+var head_right : Texture2D
+
+# Direction actuelle du serpent
+var direction = Vector2i.LEFT
+
+
+func update_head_sprite():
+	"""Met à jour le sprite de la tête du serpent selon la direction."""
+	
+	match direction:
+		Vector2i.UP: # Si le serpent va vers le haut
+			head_sprite.texture = head_up
+		
+		Vector2i.DOWN: # Si le serpent va vers le bas
+			head_sprite.texture = head_down
+		
+		Vector2i.LEFT: # vers la gauche
+			head_sprite.texture = head_left
+		
+		Vector2i.RIGHT: # ou vers la droite
+			head_sprite.texture = head_right	
+				
+
+
+func _process(float) -> void:
+	if Input.is_action_pressed("move_down"):
+		print("Déplacement vers le bas")
+		update_head_sprite()
+	
+	elif Input.is_action_pressed("move_up"):
+		print("Déplacement vers le haut")
+		update_head_sprite()
+	
+	elif Input.is_action_pressed("move_left"):
+		print("Déplacement vers la gauche")
+		update_head_sprite()
+	
+	elif Input.is_action_pressed("move_right"):
+		print("Déplacement vers la droite")
+		update_head_sprite()
+	
+	else:
+		print("Aucun déplacement")			
+	
+	
+	print(get_local_mouse_position())	
+
+func _ready():
+	pass
