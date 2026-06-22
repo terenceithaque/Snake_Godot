7f5a6663fd4616fcec7645c82bc28fc9f68633da
Terence Le Thierry <terence.lethierry@protonmail.com>
2026-06-22 08:23:20 +0200

    Lancement de la scène du jeu quand le joueur clique sur Jouer

--- title_screen.gd
+++ title_screen.gd
@@ -18,7 +18,9 @@ func _process(delta: float) -> void:
 
 
 func _on_play_button_pressed() -> void:
-	pass # Replace with function body.
+	
+	# Remplacer la scène par celle du jeu
+	get_tree().change_scene_to_file("res://game.tscn")
 
 
 func _on_quit_button_pressed() -> void:
