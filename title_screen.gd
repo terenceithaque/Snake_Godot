f9521902d84606a35133259c69eb4f944b43a377
Terence Le Thierry <terence.lethierry@protonmail.com>
2026-06-22 08:21:11 +0200

    Fermeture du jeu quand le joueur clique sur Quitter

--- title_screen.gd
+++ title_screen.gd
@@ -9,9 +9,18 @@ func _ready() -> void:
 	var title_label = get_node("PanelContainer/VBoxContainer/Title")
 	var play_button = get_node("PanelContainer/VBoxContainer/PlayButton")
 	var quit_button = get_node("PanelContainer/VBoxContainer/QuitButton")
-
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_play_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	"""Quitte le jeu"""
+	get_tree().quit() # Quitter le jeu
